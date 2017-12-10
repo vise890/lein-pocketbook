d7fad5a263ba192da89513b0b7c98d66dea7f28d
Martino Visintin <vise890@gmail.com>
2017-12-10 15:30:20 +0000

    add ci

--- project.clj
+++ project.clj
@@ -1,6 +1,15 @@
 (defproject lein-pocketbook "0.1.4-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+
+  :description "A Leiningen plugin to add sources / javadocs to your dev dependencies so that you can jump to them in emacs."
+
+  :url "http://gitlab.com/vise890/lein-pocketbook"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :profiles {:ci  {:deploy-repositories
+                   [["clojars" {:url           "https://clojars.org/repo"
+                                :username      :env
+                                :password      :env
+                                :sign-releases false}]]}})
+
   :eval-in-leiningen true)
